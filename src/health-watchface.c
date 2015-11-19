42cd69159da6ef37204adaacc7712e58702cf738
Mitchell Roy <mitchell@pebble.com>
2015-11-19 14:36:20 -0800

    First commit

---
+++ src/health-watchface.c
@@ -0,0 +1,31 @@
+#include <pebble.h>
+
+static Window *s_window;
+
+static void window_load(Window *window) {
+
+}
+
+static void window_unload(Window *window) {
+
+}
+
+static void init(void) {
+  s_window = window_create();
+  window_set_window_handlers(s_window, (WindowHandlers) {
+    .load = window_load,
+    .unload = window_unload,
+  });
+  const bool animated = true;
+  window_stack_push(s_window, animated);
+}
+
+static void deinit(void) {
+  window_destroy(s_window);
+}
+
+int main(void) {
+  init();
+  app_event_loop();
+  deinit();
+}
